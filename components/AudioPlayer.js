b7e2d86800197398e53e414047ed96232190ea19
Niels Swinkels <bitbucket@nielsswinkels.nl>
2016-05-04 14:38:19 +0200

    audio seekbar that shows the current time, and changes the currenttime when you drag it. Needs better layout

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -2,6 +2,7 @@ import React, {
   Text,
   View,
   TouchableHighlight,
+  Slider,
 } from 'react-native';
 
 import styles from '../styles/styles';
@@ -33,19 +34,30 @@ export default React.createClass({
       }});
   },
   handlePress() {
+    this.state.sound.getCurrentTime((seconds) => this.setState({ time: seconds }))
     if (this.state.isPlaying){
       this.state.sound.pause();
       this.setState({isPlaying: false});
+      clearInterval(this.interval);
     } else {
       this.state.sound.play((success) => {
         this.setState({isPlaying: false});
+        clearInterval(this.interval);
         if (success) {
           console.log('successfully finished playing');
         } else {
           console.log('playback failed due to audio decoding errors');
         }});
       this.setState({isPlaying: true});
+      this.interval = setInterval(this.tick, 100);
     }
+  },
+  componentWillUnmount: function componentWillUnmount() {
+    clearInterval(this.interval);
+    this.state.sound.stop()
+  },
+  tick: function tick() {
+    this.state.sound.getCurrentTime((seconds) => this.setState({ time: seconds }))
   },
   render() {
     if (this.state.sound === null) {
@@ -56,11 +68,16 @@ export default React.createClass({
     else {
       return (
         <TouchableHighlight onPress={this.handlePress}>
-          <View><Icon name={this.state.isPlaying? 'pause':'play'} size={60} color={'black'} />
+          <View>
+            <Icon name={this.state.isPlaying? 'pause':'play'} size={60} color={'black'} />
+            <Slider value={this.state.time} maximumValue={this.state.sound.getDuration()} onValueChange={(value) => this.state.sound.setCurrentTime(value)} />
             <Text>Duration:{Math.round(this.state.sound.getDuration())} seconds
               Playing:{this.state.isPlaying? 'yes':'no'}
-            </Text></View>
+              Current time: {this.state.time}
+            </Text>
+          </View>
         </TouchableHighlight>
+
       );}
   },
 }
