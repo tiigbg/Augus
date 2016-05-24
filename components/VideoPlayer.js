98f2bb82e417d28599e1f77767348647969b416b
Jacob Michelsen <chromegadget@gmail.com>
2016-05-24 16:06:42 +0200

    Video player controls

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -2,8 +2,10 @@ import React, {
   Text,
   View,
   TouchableHighlight,
+  Slider,
 } from 'react-native';
 
+import { secondsToTime } from '../util/time';
 import styles from '../styles/styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -13,18 +15,24 @@ export default React.createClass({
   getDefaultProps() {
     return {
       time: 0,
+      duration: 100,
       isPlaying: false,
     };
   },
-
   getInitialState() {
     return {
       time: this.props.time,
+      duration: this.props.duration,
       isPlaying: this.props.isPlaying,
     };
   },
   handlePress() {
     this.setState({ isPlaying: !this.state.isPlaying });
+  },
+  onEnd() {
+    this.setState({ isPlaying: false });
+    this.refs.videoPlayer.seek(0);
+    this.setState({ time: 0 });
   },
   render() {
     return (
@@ -32,23 +40,33 @@ export default React.createClass({
         <TouchableHighlight onPress={this.handlePress}>
           <View>
             <Icon name={this.state.isPlaying? 'pause':'play'} size={60} color={'black'} />
-            <Text>
-              Playing:{this.state.isPlaying? 'yes':'no'}
-            </Text>
           </View>
         </TouchableHighlight>
-        <Video style={styles.video} source={{uri: this.props.file}} // Can be a URL or a local file.
-          rate={1.0}                   // 0 is paused, 1 is normal.
-          volume={1.0}                 // 0 is muted, 1 is normal.
-          muted={false}                // Mutes the audio entirely.
-          paused={!this.state.isPlaying}               // Pauses playback entirely.
-          repeat={true}                // Repeat forever.
-          onLoadStart={this.loadStart} // Callback when video starts to load
-          onLoad={this.setDuration}    // Callback when video loads
-          onProgress={this.setTime}    // Callback every ~250ms with currentTime
-          onEnd={this.onEnd}           // Callback when playback finishes
-          onError={this.videoError}    // Callback when video cannot be loaded
-          style={styles.video} />
+        <View style={{ flex:1}}>
+          <View style={{ flexDirection:'row', justifyContent:'space-between' }}>
+            <Text>{secondsToTime(Math.round(this.state.time))}</Text>
+            <Text>
+              {secondsToTime(Math.round(this.state.duration - this.state.time))}
+            </Text>
+          </View>
+          <Slider value={this.state.time}
+            maximumValue={this.state.duration}
+            onValueChange={(value) => (this.refs.videoPlayer.seek(value),
+                this.setState({ time: value }))}
+          />
+          <Video ref="videoPlayer" style={styles.video} source={{uri: this.props.file}} // Can be a URL or a local file.
+            rate={1.0}                   // 0 is paused, 1 is normal.
+            volume={1.0}                 // 0 is muted, 1 is normal.
+            muted={false}                // Mut
+            paused={!this.state.isPlaying}               // Pauses playback entirely.
+            repeat={false}                // Repeat forever.
+            onLoadStart={this.loadStart} // Callback when video starts to load
+            onLoad={(videoStats) => this.setState({duration:videoStats.duration})}    // Callback when video loads
+            onProgress={(progressStats) => this.setState({time:progressStats.currentTime})}    // Callback every ~250ms with currentTime
+            onEnd={this.onEnd}           // Callback when playback finishes
+            onError={this.videoError}    // Callback when video cannot be loaded
+            style={styles.video} />
+        </View>
       </View>
     );
   },
