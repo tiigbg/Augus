32d10d9b33eb9b97b4d99eb7db08e89a1e61f7e8
Niels Swinkels <bitbucket@nielsswinkels.nl>
2016-09-06 14:57:04 +0200

    Disable warnings for during the usertest

--- Augus.js
+++ Augus.js
@@ -18,13 +18,13 @@ const scenes = Actions.create(
   <Scene key="modal" component={Modal} >
     <Scene key="root" hideNavBar>
       <Scene key="tabbar" tabs default="tab1" >
-        <Scene key="tab1" title="Stories" icon={TabIcon} icontype="leaf">
+        <Scene key="tab1" title="Utställningar" icon={TabIcon} icontype="leaf">
           <Scene key="experiment" component={Experiment} title="Experiment"  />
           <Scene key="exhibitonList" component={ExhibitionList} title="Utställningar" initial />
           <Scene key="stationList" component={StationList} title="Station List" />
           <Scene key="stationScreen" component={StationScreen} title="StationScreen" />
         </Scene>
-        <Scene key="tab2" title="Nearby" icon={TabIcon} icontype="map">
+        <Scene key="tab2" title="Karta" icon={TabIcon} icontype="map">
           <Scene key="map" component={Map} title="Nearby Stories" initial />
         </Scene>
       </Scene>
@@ -38,6 +38,8 @@ export default class Augus extends React.Component {
     this.state = { store: configureStore() };
   }
   render() {
+    // FIXME remove this after the usertest
+    console.disableYellowBox = true;
     return (
       <Provider store={this.state.store}>
         <RouterWithRedux scenes={scenes} />
