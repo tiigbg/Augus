ac65abb73be96a1c41c1e1cb5c57529a543715c4
Niels Swinkels <bitbucket@nielsswinkels.nl>
2016-12-09 15:57:14 +0100

    Show progressbar when loading audio file. iOS version not tested, but I assume it should work.

--- containers/StationList.js
+++ containers/StationList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, Image, ListView, TouchableHighlight, Text, View, TouchableOpacity } from 'react-native';
+import { ProgressBarAndroid, ProgressViewIOS, ScrollView, Image, ListView, TouchableHighlight, Text, View, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import styles from '../styles/styles';
@@ -74,34 +74,26 @@ const StationList = React.createClass({
     myDataSource = myDataSource.cloneWithRowsAndSections(dataBlob, sectionIDs, rowIDs);
 
     let hasAudio = false;
+    let audioLoaded = false;
     let audioFilename = '';
     let audioFile = this.props.audio.find((item)=>{ return item.parent_id == this.props.node.id && item.language=='sv'; });
-    console.log(audioFile);
     if (typeof audioFile !== "undefined") {
-      
-      console.log(hasAudio);
-      console.log('hasaudio');
+      hasAudio = true;
       // download audio file and save in state
       RNFetchBlob
       .config({
         fileCache : true,
-        // by adding this option, the temp files will have a file extension
-        appendExt : 'mp3'
+        appendExt : 'mp3' // FIXME should this be fixated to always be mp3?
       })
       .fetch('GET', 'http://192.168.1.121:8000/audiofiles/'+audioFile.file, {
         
       })
       .progress((received, total) => {
-        console.log('progress', received / total);
+        this.setState({ audioLoadProgress: received / total });
       })
       .then((res) => {
-        // the temp file path with file extension `png`
-        console.log('The file saved to ', res.path())
-        // Beware that when using a file path as Image source on Android,
-        // you must prepend "file://"" before the file path
-        // audioFilename = Platform.OS === 'android' ? 'file://' + res.path()  : '' + res.path();
         audioFilename = res.path();
-        this.setState({ audioFilename, hasAudio: true });
+        this.setState({ audioFilename, audioLoaded: true });
         console.log(this.state.audioFilename);
       })
       .catch((err) => {
@@ -114,6 +106,7 @@ const StationList = React.createClass({
       myDataSource,
       renderSectionHeaders,
       hasAudio,
+      audioLoaded,
       audioFilename,
     };
   },
@@ -304,18 +297,28 @@ const StationList = React.createClass({
         </ScrollView>
       );
     }
-    
     if(this.state.hasAudio) {
-      audioPlayerView = (
-        <View>
-          <View style={styles.separator} />
-          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
-            <View style={{ flex: 1, flexDirection: 'column' }}>
-              <AudioPlayer file={ this.state.audioFilename } />
+      if(this.state.audioLoaded) {
+        audioPlayerView = (
+          <View>
+            <View style={styles.separator} />
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+              <View style={{ flex: 1, flexDirection: 'column' }}>
+                <AudioPlayer file={ this.state.audioFilename } />
+              </View>
             </View>
           </View>
-        </View>
-      );
+        );
+      }
+      else {        
+        if (Platform.OS === 'android') {
+          audioPlayerView = (<ProgressBarAndroid progress={this.state.audioLoadProgress}  styleAttr='Horizontal'></ProgressBarAndroid>)
+        }
+        else
+        {
+          audioPlayerView = (<ProgressViewIOS progress={this.state.audioLoadProgress} progressViewStyle='bar'></ProgressViewIOS >);
+        }
+      }
     }
     if ('signlanguage' in this.props.node && !!this.props.node.signlanguage.sv && this.props.node.signlanguage.sv != '-') {
       signlanguageView = (
