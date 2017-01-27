0a432e9a6d5a1409a5c340fa9cecbb91e21d01bb
nielsswinkels <niels.sw@gmail.com>
2017-01-27 14:56:38 +0100

    Remove (unused) maps and adventures

--- Augus.js
+++ Augus.js
@@ -4,10 +4,10 @@ import { Modal, Actions, Scene, Router } from 'react-native-router-flux';
 
 import TabIcon from './components/TabIcon.js';
 import ExhibitionList from './containers/ExhibitionList';
-import Experiment from './containers/Experiment';
+//import Experiment from './containers/Experiment';
 import StationList from './containers/StationList';
-import StationScreen from './components/StationScreen';
-import Map from './containers/Map';
+
+
 
 
 import { configureStore } from './store/Store.js';
@@ -18,16 +18,13 @@ const scenes = Actions.create(
   // <Scene key="tabbar" tabs default="tab1" >
   // <Scene key="tab1" title="Utställningar" icon={TabIcon} icontype="leaf">
   // </Scene>
-  // <Scene key="tab2" title="Karta" icon={TabIcon} icontype="map">
-  // <Scene key="map" component={Map} title="Nearby Stories" initial />
-  // </Scene>
   // </Scene>
   // <Scene key="experiment" component={Experiment} title="Experiment"  />
   // <Scene key="modal" component={Modal} >
+//      <Scene key="stationScreen" component={StationScreen} hideNavBar  title="StationScreen" />
     <Scene key="root">
       <Scene key="exhibitonList" component={ExhibitionList} hideNavBar  title="Utställningar" initial />
       <Scene key="stationList" component={StationList} hideNavBar  title="Station List" />
-      <Scene key="stationScreen" component={StationScreen} hideNavBar  title="StationScreen" />
     </Scene>
   // </Scene>
 );
--- reducers/index.js
+++ reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 import Routes from './Routes';
 import Exhibitions from './Exhibitions';
 import Settings from './Settings';
-import Map from './Map';
 
 const rootReducer = combineReducers({
   routes: Routes,
   exhibitions: Exhibitions,
-  map: Map,
   settings: Settings,
 });
 
