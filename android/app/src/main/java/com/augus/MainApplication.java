37820c9608c0df5f486f8b2b56ca86668e4443eb
nielsswinkels <niels.sw@gmail.com>
2017-02-16 14:01:36 +0100

    Making iOS app work again.

--- android/app/src/main/java/com/augus/MainApplication.java
+++ android/app/src/main/java/com/augus/MainApplication.java
@@ -4,11 +4,11 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.RNFetchBlob.RNFetchBlobPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.reactnative.photoview.PhotoViewPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,11 +30,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFetchBlobPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage(),
             new RNSoundPackage(),
-            new PhotoViewPackage()
+            new PhotoViewPackage(),
+            new RNFetchBlobPackage()
       );
     }
   };
