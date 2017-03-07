c6f87288e1ba2fcb9f107ec708fa435f6cf32be2
nielsswinkels <niels.sw@gmail.com>
2017-03-07 16:21:26 +0100

    remember language if already set, and skip first view

--- containers/LanguageSelect.js
+++ containers/LanguageSelect.js
@@ -14,8 +14,43 @@ const LanguageSelect = React.createClass({
     return {
     };
   },
- 
+  componentWillMount()
+  {
+    console.log(this.props.navigation.state.key);
+    if(this.props.navigation.state.key == 'Init')
+    {
+      console.log('First view');
+      storage.load({
+        key: 'language',
+
+        // autoSync(default true) means if data not found or expired,
+        // then invoke the corresponding sync method
+        autoSync: false,
+
+        // syncInBackground(default true) means if data expired,
+        // return the outdated data first while invoke the sync method.
+        // It can be set to false to always return data provided by sync method when expired.(Of course it's slower)
+        syncInBackground: false
+      }).then(ret => {
+        // found data go to then()
+        this.props.changeLanguage(ret.language, ret.displaySignlanguage);
+        this.props.navigation.navigate('ExhibitionList', {title: findExhibitionListTitle(ret.language)} );
+        
+      }).catch(err => {
+        switch (err.name) {
+          case 'NotFoundError':
+              console.log('language NotFoundError');
+              break;
+          case 'ExpiredError':
+            console.log('language ExpiredError');
+              break;
+        console.warn(err.message);
+        }
+      })
+    }
+  },
   render() {
+
    
     return (
       <View style={styles.screenContainer}>
--- reducers/Settings.js
+++ reducers/Settings.js
@@ -15,6 +15,16 @@ export default function Settings(state = initialState, action) {
         baseUrl: action.payload.baseUrl
       })
     case AT.CHANGE_LANGUAGE:
+      global.storage.save({
+        key: 'language',   // Note: Do not use underscore("_") in key!
+        rawData: { 
+            language: action.language,
+            displaySignlanguage: action.displaySignlanguage,
+        },
+        // if not specified, the defaultExpires will be applied instead.
+        // if set to null, then it will never expire.
+        expires: null
+      });
       return Object.assign({}, state, {
         language: action.language,
         displaySignlanguage: action.displaySignlanguage,
