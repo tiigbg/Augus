966997b9ef1b1f2935dba13abd99ceed578b602d
nielsswinkels <niels.sw@gmail.com>
2017-09-27 19:28:13 +0200

    put back video fullscreen. Still need to add rotate to landscape though.

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View, Image, TouchableHighlight, Slider } from 'react-native';
 
 import { secondsToTime } from '../util/time';
-//import Lightbox from 'react-native-lightbox';
+import Lightbox from 'react-native-lightbox';
 
 import styles from '../styles/styles';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -47,36 +47,35 @@ export default React.createClass({
     this.setState({ isPlaying: !this.state.isPlaying });
   },
   render() {
-      /*<Lightbox
-        navigator={this.props.navigator}
-        activeProps={{ style: styles.lightBox }}
-        style={styles.colStretch}
-        onClose={this.onFullscreenClose}
-        onOpen={this.onFullscreenOpen}
-      </Lightbox>
-      >*/
     return (
         <View style={styles.colStretch}>
-          <Video
-            ref="videoPlayer"
-            source={{ uri: this.props.file }} // Can be a URL or a local file.
-            rate={1.0}                   // 0 is paused, 1 is normal.
-            volume={1.0}                 // 0 is muted, 1 is normal.
-            muted={false}                // Mut
-            resizeMode="contain"
-            paused={!this.state.isPlaying}               // Pauses playback entirely.
-            repeat={false}                // Repeat forever.
-            onLoadStart={this.loadStart} // Callback when video starts to load
-            onLoad={
-              (videoStats) => this.setState({ duration: videoStats.duration })
-            }    // Callback when video loads
-            onProgress={
-              (progressStats) => {if (this.state.isPlaying) {this.setState({ time: progressStats.currentTime });}}
-            }    // Callback every ~250ms with currentTime
-            onEnd={this.onEnd}           // Callback when playback finishes
-            onError={this.videoError}    // Callback when video cannot be loaded
-            style={this.state.isPlaying ? (styles.video) : { height: 0 }}
-          />
+          <Lightbox
+            navigator={this.props.navigator}
+            activeProps={{ style: styles.lightBox }}
+            style={styles.colStretch}
+            onClose={this.onFullscreenClose}
+            onOpen={this.onFullscreenOpen} >
+            <Video
+              ref="videoPlayer"
+              source={{ uri: this.props.file }} // Can be a URL or a local file.
+              rate={1.0}                   // 0 is paused, 1 is normal.
+              volume={1.0}                 // 0 is muted, 1 is normal.
+              muted={false}                // Mut
+              resizeMode="contain"
+              paused={!this.state.isPlaying}               // Pauses playback entirely.
+              repeat={false}                // Repeat forever.
+              onLoadStart={this.loadStart} // Callback when video starts to load
+              onLoad={
+                (videoStats) => this.setState({ duration: videoStats.duration })
+              }    // Callback when video loads
+              onProgress={
+                (progressStats) => {if (this.state.isPlaying) {this.setState({ time: progressStats.currentTime });}}
+              }    // Callback every ~250ms with currentTime
+              onEnd={this.onEnd}           // Callback when playback finishes
+              onError={this.videoError}    // Callback when video cannot be loaded
+              style={this.state.isPlaying ? (styles.video) : { height: 0 }}
+            />
+          </Lightbox>
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 4 }}>
             { this.props.showSignlanguageIcon ? <Image
                 source={icon_signlanguage_sv}
