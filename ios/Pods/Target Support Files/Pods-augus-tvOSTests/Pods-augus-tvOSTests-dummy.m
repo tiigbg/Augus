6d4c850da6820d75ab8eb2557b78f89a00819731
Jacob Michelsen <chromegadget@gmail.com>
2018-03-25 17:52:32 +0200

    Updated podfiles. Build works with on command line now but not in xcode

---
+++ ios/Pods/Target Support Files/Pods-augus-tvOSTests/Pods-augus-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_augus_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_augus_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-augus/Pods-augus-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_augus : NSObject
+@end
+@implementation PodsDummy_Pods_augus
+@end
