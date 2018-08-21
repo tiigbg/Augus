0e8234a72ef5de8e5f46ab19621d3cb43421824c
Jacob Michelsen <chromegadget@gmail.com>
2018-08-21 09:30:24 +0200

    Fix for duplicate symbol linker error when building debug

---
+++ ios/pod_post_install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd ./Pods/GVRSDK/Libraries/
+lipo -info libGVRSDK.a
+
+architectures="armv7 i386 x86_64 arm64"
+for arch in $architectures
+do
+    echo Create thin archive libGVRSDK_$arch
+    lipo -thin $arch libGVRSDK.a -output libGVRSDK_$arch
+    chmod 777 libGVRSDK_$arch
+
+    remove=""
+    for object in $(ar -t libGVRSDK_$arch)
+    do
+        if [[ ${object} == vlog_is_on_* ]]
+        then
+            echo $object
+            remove=$object
+        fi
+    done
+
+    if [ $remove ]
+    then
+        echo removing $remove from $arch
+        ar -dv libGVRSDK_$arch $remove
+    else
+        echo No symbol matching "vlog_is_on_*" found for $arch
+    fi
+
+done
+
+echo Rebuild libGVRSDK.a
+lipo -create libGVRSDK_armv7 libGVRSDK_i386 libGVRSDK_x86_64 libGVRSDK_arm64 -output libGVRSDK.a
+
+for arch in $architectures
+do
+    rm libGVRSDK_$arch
+    echo Delete thin archive libGVRSDK_$arch
+done
+
+cd ../../../
