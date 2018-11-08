0ae18133c53e11b52475a38ce0ed7cc933dc704b
Erik Einebrant <spam.erik@einebrant.se>
2018-11-08 17:38:00 +0100

    Added constant file with some language codes

---
+++ constants/LanguageCodes.js
@@ -0,0 +1,17 @@
+//https://www.loc.gov/standards/iso639-2/php/code_list.php
+export const languageCodes = [
+  {
+    code: 'sv',
+    language: 'Svenska',
+  }, {
+    code: 'en',
+    language: 'English',
+  }, {
+    code: 'es',
+    language: 'Español',
+  },
+]
+
+export default {
+  languageCodes,
+}
