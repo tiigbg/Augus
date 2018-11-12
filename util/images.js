37ba5bcad9cc5e99fe936d7c33fef2f3b1c20031
Erik Einebrant <spam.erik@einebrant.se>
2018-11-12 14:56:35 +0100

    Removed unused files for hard coded solution to using camera markers in last test

--- util/images.js
+++
@@ -1,6 +0,0 @@
-export const imageList = {
-  cathedral_marker: require('../res/cathedral_marker.jpg'),
-  stenbock_marker: require('../res/stenbock_marker.jpg'),
-  christopher_marker: require('../res/christopher_marker.jpg'),
-  porch_marker: require('../res/porch_marker.jpg'),
-}
