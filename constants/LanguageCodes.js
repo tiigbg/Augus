52a031f0d86841c75a7ec2e6bce67ffb9228b878
Erik Einebrant <spam.erik@einebrant.se>
2018-11-12 16:56:00 +0100

    Added some localization variables to the LanguageCodes constants, to be used for static button texts

--- constants/LanguageCodes.js
+++ constants/LanguageCodes.js
@@ -3,12 +3,21 @@ export const languageCodes = [
   {
     code: 'sv',
     language: 'Svenska',
+    selectText: 'Välj språk',
+    reloadText: 'Ladda om',
+    scanText: 'Scanna',
   }, {
     code: 'en',
     language: 'English',
+    selectText: 'Choose language',
+    reloadText: 'Reload',
+    scanText: 'Scan',
   }, {
     code: 'es',
     language: 'Español',
+    selectText: 'Eligir idioma',
+    reloadText: 'Recargar',
+    scanText: 'Escanear',
   },
 ]
 
