a325a465ec0e90b024f22c014bbdf07c02699249
Erik Einebrant <spam.erik@einebrant.se>
2018-11-13 18:50:35 +0100

    Started editing stylesheet, adding variables for things like colors, margins and fonts

--- styles/styles.js
+++ styles/styles.js
@@ -1,79 +1,112 @@
-import {
-  StyleSheet,
-} from 'react-native';
+import { StyleSheet, } from 'react-native';
+
+let buttonTextColor = '#8c1b1b';
+let readTextColor = 'white';
+let iconColor = '#8c1b1b';
+let backColor = 'white';
+
+let backgroundColor = '#885959';
+let componentColor = 'white';
+
+let hMargin = 32;
+let vMargin = 8;
+
+let backColor3 = 'red';
+let backColor4 = 'purple';
+let backColor5 = 'yellow';
+
+let textFont = 'Foundry Sterling W01';
 
 export default StyleSheet.create({
+  /* textColor = 'red',
+  backColor = 'blue',
+  backColor2 = 'green',
+  backColor3 = 'silver', */
+
   navigator: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: backColor,
   },
+
   screenContainer: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    backgroundColor: '#f0f0f0',
+    backgroundColor: backgroundColor,
   },
+
   body_container: {
     flexGrow: 80,
   },
+
   listMargin: {
     marginTop: 5,
     marginBottom: 0,
   },
+
   container: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: backColor3,
   },
+
   rightContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'flex-start',
     // backgroundColor: '#333333',
   },
+
   listCategoryContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#e1057d',
+    backgroundColor: backColor4,
     marginBottom: 16,
     marginRight: 8,
     marginLeft: 8,
     padding: 4,
   },
+
   listContainer: {
-    backgroundColor: '#007da0',
-    marginBottom: 16,
+    backgroundColor: componentColor,
+    marginVertical: vMargin,
+    marginHorizontal: hMargin - 8,
+    /* marginBottom: 16,
     marginRight: 8,
-    marginLeft: 8,
+    marginLeft: 8, */
     padding: 8,
-    height: 100,
+    //height: 320,
     borderRadius: 5,
   },
+
   listText: {
     flex: 1,
     flexDirection: 'row',
     fontSize: 28,
     textAlign: 'left',
     justifyContent: 'center',
-    color: '#fff',
+    color: buttonTextColor,
   },
+
   listCategoryText: {
     flex: 1,
     flexDirection: 'row',
     fontSize: 28,
     textAlign: 'center',
     justifyContent: 'center',
-    color: '#fff',
+    color: buttonTextColor,
   },
+
   tabIcon: {
     flex: 1,
     flexDirection: 'column',
     alignItems: 'center',
   },
+
   map: {
     position: 'absolute',
     top: 0,
@@ -81,36 +114,44 @@ export default StyleSheet.create({
     right: 0,
     bottom: 0,
   },
+
   thumbnail: {
     width: 120,
     height: 81,
   },
+
   title: {
     fontSize: 20,
     marginBottom: 8,
     textAlign: 'center',
   },
+
   year: {
     textAlign: 'center',
   },
+
   section_name: {
     fontSize: 20,
     marginBottom: 4,
     textAlign: 'center',
   },
+
   section_name_container: {
     backgroundColor:"#e1057d",
     marginBottom: 4,
   },
+
   navbar_container: {
     flexGrow: 1,
   },
+
   stationTitlePane: {
     justifyContent: 'space-between',
     flexGrow: 1,
     flexDirection: 'row',
     marginBottom: 5,
   },
+
   station_name: {
     fontSize: 32,
     margin: 0,
@@ -121,6 +162,7 @@ export default StyleSheet.create({
     flexWrap: 'wrap',
     flex: 1,
   },
+
   backButton: {
     fontSize: 32,
     margin: 5,
@@ -128,41 +170,49 @@ export default StyleSheet.create({
     marginLeft: 25,
     textAlign: 'center',
     fontWeight: '500',
-    color: 'black',
+    color: buttonTextColor,
   },
+
   station_text: {
     fontSize: 22,
-    color: '#000',
+    color: readTextColor,
     marginTop: 0,
     marginBottom: 14,
   },
+
   listView: {
     paddingTop: 64,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: backColor3,
   },
+
   contentContainer: {
     padding: 10,
     paddingTop: 5,
     //paddingBottom: 50,
   },
+  
   rightPane: {
     justifyContent: 'space-between',
     flex: 1,
   },
+
   adventureTitle: {
     flex: 1,
     fontSize: 24,
     fontWeight: '500',
   },
+
   separator: {
     backgroundColor: 'rgba(0, 0, 0, 0.1)',
     height: StyleSheet.hairlineWidth,
     marginVertical: 10,
   },
+
   imageDescription: {
     fontSize: 22,
-    color: '#000',
+    color: readTextColor,
   },
+
   expandIconBox: {
     width: 200,
     flexDirection: 'row',
@@ -171,10 +221,11 @@ export default StyleSheet.create({
     marginBottom:0,
     marginRight:-75,
   },
+
   expandIcon: {
     fontSize: 48,
-    color: '#000',
-    backgroundColor: '#ffffff',
+    color: iconColor,
+    backgroundColor: backColor,
     borderWidth: 3,
     borderRadius: 5,
     padding: 10,
@@ -182,41 +233,47 @@ export default StyleSheet.create({
     paddingRight: 5,
     marginRight: 5,
   },
+
   collapseIconTouch:{
     borderWidth:3,
     borderColor:'black',
     borderRadius:5,
-    backgroundColor:'white',
+    backgroundColor: iconColor,
   },
+
   collapseIcon: {
     fontSize: 35,
-    color: '#000',
+    color: readTextColor,
     padding: 10,
   },
+
   detailsImage: {
     width: 200,
     height: 200,
     backgroundColor: '#eaeaea',
   },
+
   closeButton: {
-    color: 'white',
+    color: iconColor,
     textAlign: 'center',
     margin: 10,
     // alignSelf: 'center',
     fontSize: 60,
   },
+
   closeButtonContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 8,
-    borderColor: 'white',
-    borderRadius: 5,
+    //borderColor: backColor,
+    //borderRadius: 5,
     margin: 5,
     alignSelf: 'flex-start',
-    backgroundColor: '#000',
+    backgroundColor: backColor,
   },
+
   playPauseButton: {
     fontSize: 40,
     borderWidth:3,
@@ -227,35 +284,39 @@ export default StyleSheet.create({
     paddingLeft: 10,
     paddingTop: 8,
     marginRight:5,
-    backgroundColor:'white',
+    backgroundColor: backColor,
   },
+
   exhibitionImage: {
     flex: 1,
     flexDirection: 'row',
-    alignItems: 'stretch',
-    height: 100,
-    backgroundColor: '#eaeaea',
-    borderWidth: 3,
-    borderColor: '#007da0',
-    marginRight: -10,
+    //alignItems: 'stretch',
+    height: 160,
+    //backgroundColor: backColor5,
+    //borderWidth: 3,
+    //borderColor: '#007da0',
+    /* marginRight: -10,
     marginLeft: -10,
-    marginTop: -10,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    padding: 8,
+    marginTop: -10, */
+    //borderTopLeftRadius: 10,
+    //borderTopRightRadius: 10,
+    //padding: 8,
   },
+
   stationSymbol: {
     height: 40,
     width: 40,
     marginRight: 10,
 
   },
+
   imageGallery: {
     paddingTop: 10,
     paddingBottom: 10,
     flexDirection: 'row',
     backgroundColor: '#888',
   },
+
   imageGalleryBox: {
     justifyContent: 'center',
     flexDirection:'column', 
@@ -269,10 +330,12 @@ export default StyleSheet.create({
     backgroundColor: '#eee',
     borderRadius: 10,
   },
+
   imageCaption: { 
     flexGrow: 1,
     borderWidth: 10,
   },
+
   backgroundVideo: {
     position: 'absolute',
     top: 0,
@@ -280,16 +343,38 @@ export default StyleSheet.create({
     bottom: 0,
     right: 0,
   },
+
   colStretch: {
     flexDirection: 'column',
     alignItems: 'stretch',
   },
+
   video: {
     height: 210,
     alignSelf: 'stretch',
   },
+
   lightBox: {
     flex: 1,
     height: 150,
+  },
+
+  bannerButton: {
+    backgroundColor: componentColor,
+    marginVertical: vMargin,
+    marginLeft: hMargin,
+    marginRight: hMargin,
+    padding: 16,
+    height: 320,
+    borderRadius: 5,
+  },
+
+  simpleButton: {
+    backgroundColor: componentColor,
+    marginVertical: vMargin,
+    marginHorizontal: hMargin,
+    padding: 16,
+    height: 80,
+    borderRadius: 5,
   },
 });
