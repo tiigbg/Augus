d77c8247f61e5ee2dd621b89a36798641b18be57
Erik Einebrant <spam.erik@einebrant.se>
2018-11-30 10:01:22 +0100

    Added more localization to LanguageCodes

--- constants/LanguageCodes.js
+++ constants/LanguageCodes.js
@@ -1,24 +1,34 @@
 //https://www.loc.gov/standards/iso639-2/php/code_list.php
 export const languageCodes = [
   {
-    code: 'sv',
-    language: 'Svenska',
-    selectText: 'Välj språk',
-    reloadText: 'Ladda om',
-    scanText: 'Scanna',
+    code: 'de',
+    language: 'Deutsche',
+    selectText: 'Sprache wählen',
+    reloadText: 'Neu laden',
+    scanText: 'Scan',
+    modelText: '3D-modell',
   }, {
     code: 'en',
     language: 'English',
     selectText: 'Choose language',
     reloadText: 'Reload',
     scanText: 'Scan',
+    modelText: '3D Model',
   }, {
     code: 'es',
     language: 'Español',
     selectText: 'Eligir idioma',
     reloadText: 'Recargar',
     scanText: 'Escanear',
-  },
+    modelText: 'Modelo 3D',
+  }, {
+    code: 'sv',
+    language: 'Svenska',
+    selectText: 'Välj språk',
+    reloadText: 'Ladda om',
+    scanText: 'Scanna',
+    modelText: '3D-modell',
+  }, 
 ]
 
 export default {
