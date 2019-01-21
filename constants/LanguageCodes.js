d24b820b497c48f7917e1c18c0acb661ea61866e
Erik Einebrant <spam.erik@einebrant.se>
2019-01-21 11:45:38 +0100

    Localization fixes

--- constants/LanguageCodes.js
+++ constants/LanguageCodes.js
@@ -1,33 +1,40 @@
 //https://www.loc.gov/standards/iso639-2/php/code_list.php
 export const languageCodes = [
   {
+    code: 'ar',
+    language: 'عربى',
+    selectText: 'اختر اللغة',
+    reloadText: 'إعادة تحميل',
+    scanText: 'تفحص',
+    modelText: '3D عرض في',
+  }, {
     code: 'de',
     language: 'Deutsche',
     selectText: 'Sprache wählen',
     reloadText: 'Neu laden',
     scanText: 'Scan',
-    modelText: '3D-modell',
+    modelText: 'Ansicht in 3D',
   }, {
     code: 'en',
     language: 'English',
     selectText: 'Choose language',
     reloadText: 'Reload',
     scanText: 'Scan',
-    modelText: '3D Model',
+    modelText: 'View in 3D',
   }, {
     code: 'es',
     language: 'Español',
     selectText: 'Eligir idioma',
     reloadText: 'Recargar',
     scanText: 'Escanear',
-    modelText: 'Modelo 3D',
+    modelText: 'Ver en 3D',
   }, {
     code: 'sv',
     language: 'Svenska',
     selectText: 'Välj språk',
     reloadText: 'Ladda om',
     scanText: 'Scanna',
-    modelText: '3D-modell',
+    modelText: 'Visa i 3D',
   }, 
 ]
 
